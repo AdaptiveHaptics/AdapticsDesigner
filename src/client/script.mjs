eda8f2bb328657b89539555a4708173db2972363
Kevin John <58094058+kevin-cgc@users.noreply.github.com>
2022-12-20 00:30:23 -0700

    add eslint, fix line endings

--- src/client/script.mjs
+++ src/client/script.mjs
@@ -1,6 +1,5 @@
 import Split from "./thirdparty/split-grid.mjs";
-const splitgrid = /** @type {import("split-grid").default} */(/** @type {unknown} */(Split));
-
+const splitGrid = /** @type {import("split-grid").default} */(/** @type {unknown} */(Split));
 
 /** @typedef {import("../shared/types").MidAirHapticsAnimationFileFormat} MidAirHapticsAnimationFileFormat */
 
@@ -8,54 +7,58 @@ const splitgrid = /** @type {import("split-grid").default} */(/** @type {unknown
 
 /** @type {{filename: string, filedata: MidAirHapticsAnimationFileFormat }} */
 const current_design = {
-    filename: "",
-    filedata: {
-        revision: "0.0.1-alpha.1",
-        name: "test",
+	filename: "",
+	filedata: {
+		revision: "0.0.1-alpha.1",
+		name: "test",
 
-        direction: "normal",
-        duration: 5 * 1000,
-        iteration_count: 1,
+		direction: "normal",
+		duration: 5 * 1000,
+		iteration_count: 1,
 
-        projection: "plane",
-        update_rate: 1,
+		projection: "plane",
+		update_rate: 1,
 
-        keyframes: [
-            {
-                time: 0.000,
-                coords: {
-                    x: 0,
-                    y: 0,
-                    z: 0,
-                },
-                intensity: {
-                    name: "Constant",
-                    params: {
-                        value: 0.75000
-                    }
-                },
-                brush: {
-                    name: "Point",
-                    params: {
-                        size: 1
-                    }
-                },
-                transition: {
-                    name: "Linear",
-                    params: {}
-                }
-            }
-        ]
-    }
+		keyframes: [
+			{
+				time: 0.000,
+				coords: {
+					x: 0,
+					y: 0,
+					z: 0,
+				},
+				intensity: {
+					name: "Constant",
+					params: {
+						value: 0.75000
+					}
+				},
+				brush: {
+					name: "Point",
+					params: {
+						size: 1
+					}
+				},
+				transition: {
+					name: "Linear",
+					params: {}
+				}
+			}
+		]
+	}
 };
 
 
-splitgrid({
-    columnGutters: [
-        { track: 1, element: document.querySelector("div.gutter.leftcenter") },
-        { track: 3, element: document.querySelector("div.gutter.centerright") },
-    ],
-    rowGutters: [
-        { track: 1, element: document.querySelector("div.gutter.topbottom") },
-    ]
-})
+splitGrid({
+	columnGutters: [
+		{ track: 1, element: document.querySelector("div.gutter.leftcenter") },
+		{ track: 3, element: document.querySelector("div.gutter.centerright") },
+	],
+	rowGutters: [
+		{ track: 1, element: document.querySelector("div.gutter.topbottom") },
+	]
+});
+
+document.addEventListener("keydown", ev => {
+	if (ev.key == "/" || ev.key == "?") alert("This is the help message for now");
+});

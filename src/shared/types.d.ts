ebd2bb1c1fe23c890f73a2e7944353c5e6d0d3cd
Kevin John <58094058+kevin-cgc@users.noreply.github.com>
2022-12-21 19:18:34 -0700

    cleanup types

--- src/shared/types.d.ts
+++ src/shared/types.d.ts
@@ -64,23 +64,3 @@ const TransitionLinear = make_variant<"Linear", {}>("Linear");
 const TransitionSteps = make_variant<"Steps", {}>("Steps");
 type MAHTransition = ReturnType<typeof TransitionLinear> | ReturnType<typeof TransitionSteps>;
 
-
-
-/**** DEEP IMMUTABLE ****/
-type ImmutablePrimitive = undefined | null | boolean | string | number | Function;
-
-type Immutable<T> =
-    T extends ImmutablePrimitive ? T :
-    T extends Array<infer U> ? ReadonlyArray<U> :
-    T extends Map<infer K, infer V> ? ReadonlyMap<K, V> : Readonly<T>
-
-export type DeepImmutable<T> =
-    T extends ImmutablePrimitive ? T :
-    T extends Array<infer U> ? ImmutableArray<U> :
-    T extends Map<infer K, infer V> ? ImmutableMap<K, V> :
-    T extends Set<infer M> ? ImmutableSet<M> : ImmutableObject<T>;
-
-export type ImmutableArray<T> = ReadonlyArray<Immutable<T>>;
-export type ImmutableMap<K, V> = ReadonlyMap<Immutable<K>, Immutable<V>>;
-export type ImmutableSet<T> = ReadonlySet<Immutable<T>>;
-export type ImmutableObject<T> = { readonly [K in keyof T]: Immutable<T[K]> };
---
+++ src/shared/util.d.ts
@@ -0,0 +1,18 @@
+/**** DEEP IMMUTABLE ****/
+type ImmutablePrimitive = undefined | null | boolean | string | number | Function;
+
+type Immutable<T> =
+    T extends ImmutablePrimitive ? T :
+    T extends Array<infer U> ? ReadonlyArray<U> :
+    T extends Map<infer K, infer V> ? ReadonlyMap<K, V> : Readonly<T>
+
+export type DeepImmutable<T> =
+    T extends ImmutablePrimitive ? T :
+    T extends Array<infer U> ? ImmutableArray<U> :
+    T extends Map<infer K, infer V> ? ImmutableMap<K, V> :
+    T extends Set<infer M> ? ImmutableSet<M> : ImmutableObject<T>;
+
+export type ImmutableArray<T> = ReadonlyArray<Immutable<T>>;
+export type ImmutableMap<K, V> = ReadonlyMap<Immutable<K>, Immutable<V>>;
+export type ImmutableSet<T> = ReadonlySet<Immutable<T>>;
+export type ImmutableObject<T> = { readonly [K in keyof T]: Immutable<T[K]> };

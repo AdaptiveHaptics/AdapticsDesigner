09ae7155bd20d3adc280dec6f766e9886f51e2ab
Kevin John <58094058+kevin-cgc@users.noreply.github.com>
2023-06-13 22:18:49 -0700

    add autocomplete for dynamic-f64-input

--- src/client/js/rightpanel/common/dynamic-f64-input.mjs
+++ src/client/js/rightpanel/common/dynamic-f64-input.mjs
@@ -2,16 +2,23 @@
 /** @typedef {import("../../fe/patterndesign.mjs").MAHKeyframeFE} MAHKeyframeFE */
 /** @typedef {import("../../../../external/pattern_evaluator/rs-shared-types").MAHDynamicF64} MAHDynamicF64 */
 
-export class DynamicF64Input extends HTMLLabelElement {
+import { assert_unreachable } from "../../util.mjs";
+
+export class DynamicF64Input extends HTMLElement {
 	#_pattern_design;
 	#_name;
+	#_label = document.createElement("label");
 	#_labeltext_span = document.createElement("span");
+	#_val_input_div = document.createElement("div");
 	#_val_input = document.createElement("input");
+	#_autocomplete_div = document.createElement("div");
 	#_step = 1;
 	#_set;
 	#_get;
 	#_min;
 	#_max;
+	/** @type {Parameters<DynamicF64Input["update_value"]>[0]} */
+	#_last_value = null;
 
 	/**
 	 *
@@ -35,7 +42,7 @@ export class DynamicF64Input extends HTMLLabelElement {
 		this.#_labeltext_span.classList.add("labeltext");
 		this.#_name = name;
 		this.#_labeltext_span.textContent = name;
-		this.appendChild(this.#_labeltext_span);
+		this.#_label.appendChild(this.#_labeltext_span);
 		if (unit) {
 			const unit_span = document.createElement("span");
 			unit_span.classList.add("unit");
@@ -47,10 +54,13 @@ export class DynamicF64Input extends HTMLLabelElement {
 		this.#_min = min;
 		this.#_max = max;
 
+
+		this.#_val_input_div.classList.add("vinput");
 		this.#_val_input.type = "text";
 		if (this.#_get) this.#_val_input.value = this.#_get().value.toString();
 		this.#_val_input.addEventListener("keydown", ev => {
 			const value = this.#_parse_input_value();
+			if (ev.key === "Escape") this.#_reset_value();
 			if (value.type === "f64") {
 				if (ev.key === "ArrowUp") {
 					this.#_val_input.value = (value.value + this.#_step).toString();
@@ -65,34 +75,65 @@ export class DynamicF64Input extends HTMLLabelElement {
 		});
 		this.#_val_input.addEventListener("change", ev => {
 			ev.stopPropagation(); //i cant use shadowroot because subgrid isnt supported (and may not even work in shadow root)
+			if (this.#_val_input.value == "") this.#_reset_value();
 			this.#_on_val_input_change();
 		});
-		this.appendChild(this.#_val_input);
+		this.#_val_input_div.appendChild(this.#_val_input);
+
+		this.#_autocomplete_div.classList.add("menulist", "autocomplete");
+		this.#_val_input_div.appendChild(this.#_autocomplete_div);
+
+
+		this.#_label.appendChild(this.#_val_input_div);
+
+		this.appendChild(this.#_label);
+
+		this.#_val_input.addEventListener("input", _ev => {
+			console.log("input");
+			this.#_update_autocomplete();
+		});
 	}
 
 	get_name() {
 		return this.#_name;
 	}
 
+
 	/**
 	 *
-	 * @param {MAHDynamicF64=} v
+	 * @param {MAHDynamicF64} v
+	 * @returns
+	 */
+	static stringify_df64(v) {
+		if (v.type === "f64") {
+			return (Math.round(v.value*100000)/100000).toString();
+		} else {
+			return v.value;
+		}
+	}
+	/**
+	 *
+	 * @param {(MAHDynamicF64 | null)=} v
 	 */
 	update_value(v) {
+		this.#_last_value = v;
 		if (v === undefined) {
 			if (!this.#_get) throw new Error("Cannot auto update value without getter");
 			v = this.#_get();
 		}
 		if (v === null) {
 			this.#_val_input.value = "";
-		} else if (v.type === "f64") {
-			this.#_val_input.value = (Math.round(v.value*100000)/100000).toString();
 		} else {
-			this.#_val_input.value = v.value;
+			this.#_val_input.value = DynamicF64Input.stringify_df64(v);
 		}
+		this.#_update_autocomplete();
 	}
 	get_value() {
 		return this.#_parse_input_value();
+	}
+
+	#_reset_value() {
+		this.update_value(this.#_last_value);
 	}
 
 	/**
@@ -110,9 +151,10 @@ export class DynamicF64Input extends HTMLLabelElement {
 	 * @returns {MAHDynamicF64}
 	 */
 	#_parse_input_value() {
-		const v = parseFloat(this.#_val_input.value);
-		if (Number.isFinite(v)) {
-			return { type: "f64", value: this.#_constrain_f64(v) };
+		const v_num = Number(this.#_val_input.value);
+		const v_pf = parseFloat(this.#_val_input.value);
+		if (Number.isFinite(v_num) && v_num === v_pf) {
+			return { type: "f64", value: this.#_constrain_f64(v_num) };
 		} else {
 			return { type: "dynamic", value: this.#_val_input.value };
 		}
@@ -125,7 +167,79 @@ export class DynamicF64Input extends HTMLLabelElement {
 			this.#_pattern_design.commit_operation(this.#_set(this.#_parse_input_value()));
 		}
 	}
+
+	#_update_autocomplete() {
+		/**
+		 *
+		 * @param {MAHDynamicF64} df64v
+		 * @param {object} param0
+		 * @param {string=} param0.type
+		 * @param {boolean=} param0.autoaction
+		 */
+		const insert_autocompletion = (df64v, { type, autoaction } = {}) =>  {
+			const value = DynamicF64Input.stringify_df64(df64v);
+			if (!type) {
+				switch (df64v.type) {
+					case "f64": type = "constant"; break;
+					case "dynamic": type = "parameter"; break;
+					default: assert_unreachable(df64v);
+				}
+			}
+
+			{ //create dom elements
+				const autocompletion_button = document.createElement("button");
+				autocompletion_button.classList.add("df64autocompletion");
+				if (autoaction) autocompletion_button.classList.add("autoaction");
+				const value_span = document.createElement("span");
+				value_span.classList.add("value");
+				value_span.textContent = value;
+				autocompletion_button.appendChild(value_span);
+				const type_span = document.createElement("span");
+				type_span.classList.add("type");
+				type_span.textContent = type;
+				autocompletion_button.appendChild(type_span);
+
+				autocompletion_button.addEventListener("mousedown", ev => {
+					ev.preventDefault();
+					this.update_value(df64v);
+					this.#_on_val_input_change();
+					requestAnimationFrame(() => {
+						// console.log(document.activeElement);
+						if (document.activeElement instanceof HTMLElement) {
+							document.activeElement.blur();
+						}
+						// console.log(document.activeElement);
+					});
+				});
+
+				this.#_autocomplete_div.appendChild(autocompletion_button);
+			}
+		};
+
+		while (this.#_autocomplete_div.lastChild) this.#_autocomplete_div.removeChild(this.#_autocomplete_div.lastChild);
+
+		const df64v = this.#_parse_input_value();
+		const user_params = [...this.#_pattern_design.get_user_parameters_to_linked_map().keys()];
+
+		if (df64v.type === "f64") {
+			insert_autocompletion(df64v, { autoaction: true });
+		} else {
+			if (user_params.includes(this.#_val_input.value)) {
+				insert_autocompletion({ type: "dynamic", value: this.#_val_input.value }, { autoaction: true });
+			} else if (this.#_val_input.value !== "") {
+				insert_autocompletion({ type: "dynamic", value: this.#_val_input.value }, { type: "create new parameter", autoaction: true });
+			}
+		}
+
+		user_params.filter(p =>
+			p.toLocaleLowerCase().includes(this.#_val_input.value.toLocaleLowerCase()) &&
+			p !== this.#_val_input.value
+		).forEach(p => {
+			insert_autocompletion({ type: "dynamic", value: p });
+		});
+
+	}
 }
 
 // Register the custom element
-customElements.define("dynamic-f64-input", DynamicF64Input, { extends: "label" });
+customElements.define("dynamic-f64-input", DynamicF64Input);

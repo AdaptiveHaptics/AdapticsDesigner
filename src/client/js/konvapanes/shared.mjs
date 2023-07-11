1e56557524a2cc83d9dd1a929f8f1bce43fcc3a0
Kevin John <58094058+kevin-cgc@users.noreply.github.com>
2023-07-11 11:36:30 -0700

    fix: improve timeline scroll with zoom behavior (allow zooming in to up to 20000ms) Use hybrid approach: h_scroll_position_ms is split into h_scroll_offset_ms (requiring rerender) and scrolling_layer.x() (no rerender, higher perf scrolling) increasing `fullWidth: 4000` reduces rerender rate during scrolling, but requires more memory

--- src/client/js/konvapanes/shared.mjs
+++ src/client/js/konvapanes/shared.mjs
@@ -39,8 +39,89 @@ export class KonvaResizeStage {
 	}
 }
 
+
+const SCROLLBAR_WIDTH = 10;
+const SCROLLBAR_HEIGHT = 100;
+const SCROLLBAR_OPACITY = 0.8;
+const SCROLLBAR_PADDING = 5;
 export class KonvaResizeScrollStage extends KonvaResizeStage {
-	scrollbar_padding = 5;
+
+	scaled_stage_width() {
+		return this.k_stage.width() / this.k_stage.scaleX();
+	}
+	scaled_stage_height() {
+		return this.k_stage.height() / this.k_stage.scaleY();
+	}
+
+	vsb_x() {
+		return this.scaled_stage_width() - SCROLLBAR_PADDING - SCROLLBAR_WIDTH;
+	}
+	vsb_max_y() {
+		return this.scaled_stage_height() - this.vertical_scroll_bar.height() - SCROLLBAR_PADDING;
+	}
+	vsb_min_y() {
+		return SCROLLBAR_PADDING;
+	}
+	/** @param {number} y */
+	clamp_vsb_y(y) {
+		return Math.max(Math.min(y, this.vsb_max_y()), this.vsb_min_y());
+	}
+	vsb_range() {
+		return this.vsb_max_y() - this.vsb_min_y();
+	}
+	/** @param {number} y */
+	vsb_y_to_perc(y) {
+		return (y - this.vsb_min_y()) / this.vsb_range();
+	}
+
+	hsb_y() {
+		return this.scaled_stage_height() - SCROLLBAR_PADDING - SCROLLBAR_WIDTH;
+	}
+	hsb_max_x() {
+		return this.scaled_stage_width() - this.horizontal_scroll_bar.width() - SCROLLBAR_PADDING;
+	}
+	hsb_min_x() {
+		return SCROLLBAR_PADDING;
+	}
+	/** @param {number} x */
+	clamp_hsb_x(x) {
+		return Math.max(Math.min(x, this.hsb_max_x()), this.hsb_min_x());
+	}
+	hsb_range() {
+		return this.hsb_max_x() - this.hsb_min_x();
+	}
+	/** @param {number} x */
+	hsb_x_to_perc(x) {
+		return (x - this.hsb_min_x()) / this.hsb_range();
+	}
+
+
+	sl_min_y() {
+		return -(this.fullHeight - this.k_stage.height());
+	}
+	sl_max_y() {
+		return 0;
+	}
+	sl_perc_to_y(perc) {
+		return this.sl_min_y() + (1 - perc) * (this.sl_max_y() - this.sl_min_y());
+	}
+	sl_dy_to_dperc(dy) {
+		return dy / (this.sl_max_y() - this.sl_min_y());
+	}
+	sl_min_x() {
+		return -(this.fullWidth - this.k_stage.width());
+	}
+	sl_max_x() {
+		return 0;
+	}
+	sl_perc_to_x(perc) {
+		return this.sl_min_x() + (1 - perc) * (this.sl_max_x() - this.sl_min_x());
+	}
+	sl_dx_to_dperc(dx) {
+		return dx / (this.sl_max_x() - this.sl_min_x());
+	}
+
+
 
 	/**
 	 *
@@ -61,53 +142,45 @@ export class KonvaResizeScrollStage extends KonvaResizeStage {
 		this.k_stage.add(this.scroll_bar_layer);
 
 		this.vertical_scroll_bar = new Konva.Rect({
-			width: 10,
-			height: 100,
 			fill: getComputedStyle(document.body).getPropertyValue("--scrollbar-thumb-color"),
-			opacity: 0.8,
-			x: this.k_stage.width() - this.scrollbar_padding - 10,
-			y: this.scrollbar_padding,
+			opacity: SCROLLBAR_OPACITY,
 			draggable: true,
+
+			width: SCROLLBAR_WIDTH,
+			height: SCROLLBAR_HEIGHT,
+			x: this.vsb_x(),
+			y: this.vsb_min_y(),
 			dragBoundFunc: pos => {
-				pos.x = this.k_stage.width() - this.scrollbar_padding - 10;
-				pos.y = Math.max(
-					Math.min(pos.y, this.k_stage.height() - this.vertical_scroll_bar.height() - this.scrollbar_padding),
-					this.scrollbar_padding
-				);
+				pos.x = this.vsb_x();
+				pos.y = this.clamp_vsb_y(pos.y);
 				return pos;
 			},
 		});
 		this.scroll_bar_layer.add(this.vertical_scroll_bar);
 		this.vertical_scroll_bar.on("dragmove", () => {
-			const availableHeight = this.k_stage.height() - this.scrollbar_padding * 2 - this.vertical_scroll_bar.height();
-			var delta = (this.vertical_scroll_bar.y() - this.scrollbar_padding) / availableHeight; //delta in %
-
-			this.scrolling_layer.y(-(this.fullHeight - this.k_stage.height()) * delta);
+			const perc = this.vsb_y_to_perc(this.vertical_scroll_bar.y());
+			this.set_vscroll_perc(perc);
 		});
 
 		this.horizontal_scroll_bar = new Konva.Rect({
-			width: 100,
-			height: 10,
 			fill: getComputedStyle(document.body).getPropertyValue("--scrollbar-thumb-color"),
-			opacity: 0.8,
-			x: this.scrollbar_padding,
-			y: this.k_stage.height() - this.scrollbar_padding - 10,
+			opacity: SCROLLBAR_OPACITY,
 			draggable: true,
+
+			width: SCROLLBAR_HEIGHT,
+			height: SCROLLBAR_WIDTH,
+			x: this.hsb_min_x(),
+			y: this.hsb_y(),
 			dragBoundFunc: pos => {
-				pos.x = Math.max(
-					Math.min(pos.x, this.k_stage.width() - this.horizontal_scroll_bar.width() - this.scrollbar_padding),
-					this.scrollbar_padding
-				);
-				pos.y = this.k_stage.height() - this.scrollbar_padding - 10;
+				pos.x = this.clamp_hsb_x(pos.x);
+				pos.y = this.hsb_y();
 				return pos;
 			},
 		});
 		this.scroll_bar_layer.add(this.horizontal_scroll_bar);
 		this.horizontal_scroll_bar.on("dragmove", _pos => {
-			const availableWidth = this.k_stage.width() - this.scrollbar_padding * 2 - this.horizontal_scroll_bar.width();
-			var delta = (this.horizontal_scroll_bar.x() - this.scrollbar_padding) / availableWidth; //delta in %
-
-			this.scrolling_layer.x(-(this.fullWidth - this.k_stage.width()) * delta);
+			const perc = this.hsb_x_to_perc(this.horizontal_scroll_bar.x());
+			this.set_hscroll_perc(perc);
 		});
 
 		this.k_stage.on("wheel", ev => {
@@ -116,37 +189,29 @@ export class KonvaResizeScrollStage extends KonvaResizeStage {
 			ev.evt.preventDefault();
 			const [dx, dy] = this.flipDefaultScrollDirection != ev.evt.shiftKey ? [ev.evt.deltaY, ev.evt.deltaX] : [ev.evt.deltaX, ev.evt.deltaY];
 
-			const minX = -(this.fullWidth - this.k_stage.width());
-			const maxX = 0;
+			const perc_y = this.vscroll_dy_to_dperc(dy) + this.get_vscroll_perc();
+			this.set_vscroll_perc(perc_y);
 
-			const x = Math.max(minX, Math.min(this.scrolling_layer.x() - dx, maxX));
-
-			const minY = -(this.fullHeight - this.k_stage.height());
-			const maxY = 0;
-
-			const y = Math.max(minY, Math.min(this.scrolling_layer.y() - dy, maxY));
-			this.scrolling_layer.position({ x, y });
+			const perc_x = this.hscroll_dx_to_dperc(dx) + this.get_hscroll_perc();
+			this.set_hscroll_perc(perc_x);
 
 			this.fix_scrollbar_coords();
 		});
+
+		this.fix_scrollbar_coords();
 	}
 
 	fix_scrollbar_coords() {
 		if (!this.vertical_scroll_bar && !this.horizontal_scroll_bar) return;
 
-		const stage_width = this.k_stage.width() / this.k_stage.scaleX();
-		const stage_height = this.k_stage.height() / this.k_stage.scaleY();
+		this.vertical_scroll_bar.x(this.vsb_x());
+		this.horizontal_scroll_bar.y(this.hsb_y());
 
-		this.vertical_scroll_bar.x(stage_width - this.scrollbar_padding - 10);
-		this.horizontal_scroll_bar.y(stage_height - this.scrollbar_padding - 10);
+		const vy = this.vsb_min_y() + this.get_vscroll_perc() * this.vsb_range();
+		this.vertical_scroll_bar.y(this.clamp_vsb_y(vy));
 
-		const availableHeight = stage_height - this.scrollbar_padding * 2 - this.vertical_scroll_bar.height();
-		const vy = (this.scrolling_layer.y() / (-this.fullHeight + stage_height)) * availableHeight + this.scrollbar_padding;
-		this.vertical_scroll_bar.y(vy);
-
-		const availableWidth = stage_width - this.scrollbar_padding * 2 - this.horizontal_scroll_bar.width();
-		const hx = (this.scrolling_layer.x() / (-this.fullWidth + stage_width)) * availableWidth + this.scrollbar_padding;
-		this.horizontal_scroll_bar.x(hx);
+		const hx = this.hsb_min_x() + this.get_hscroll_perc() * this.hsb_range();
+		this.horizontal_scroll_bar.x(this.clamp_hsb_x(hx));
 	}
 
 	fitStageIntoParentContainer() {
@@ -154,5 +219,34 @@ export class KonvaResizeScrollStage extends KonvaResizeStage {
 		this.k_stage.height(this.resize_container.offsetHeight);
 
 		this.fix_scrollbar_coords();
+	}
+
+
+
+	/****  Overridable scroll functions  ****/
+
+	/** @param {number} perc */
+	set_vscroll_perc(perc) {
+		perc = Math.max(0, Math.min(1, perc));
+		this.scrolling_layer.y(this.sl_perc_to_y(perc));
+	}
+	get_vscroll_perc() {
+		return this.scrolling_layer.y() / (-this.fullHeight + this.k_stage.height());
+	}
+	vscroll_dy_to_dperc(dy) {
+		return this.sl_dy_to_dperc(dy);
+	}
+
+
+	/** @param {number} perc */
+	set_hscroll_perc(perc) {
+		perc = Math.max(0, Math.min(1, perc));
+		this.scrolling_layer.x(this.sl_perc_to_x(perc));
+	}
+	get_hscroll_perc() {
+		return this.scrolling_layer.x() / (-this.fullWidth + this.k_stage.width());
+	}
+	hscroll_dx_to_dperc(dx) {
+		return this.sl_dx_to_dperc(dx);
 	}
 }
